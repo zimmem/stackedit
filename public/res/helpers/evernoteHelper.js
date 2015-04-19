26fe3721c56d97a3f9bd45b76315351ada86cb2b
Zimmem <zimmem@gmail.com>
2015-04-19 16:32:23 +0100

    commit

---
+++ public/res/helpers/evernoteHelper.js
@@ -0,0 +1,10 @@
+define([ "jquery", "constants", "core", "utils", "storage", "logger",
+		"eventMgr", "classes/AsyncTask" ], function($, C, core, utils, storage, log, eventMgr, AsyncTask) {
+	
+	var evernoteHelper = {};
+	
+	evernoteHelper.upload = function(title, content, callback){
+		
+	}
+
+});
